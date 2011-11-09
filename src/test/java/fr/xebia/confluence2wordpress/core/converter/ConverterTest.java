b666a549c7569f5664dacda61eeac786cc889479
adutra <alexdut@gmail.com>
2011-11-09 21:27:33 +0100

    Fixing test fixture.

--- src/test/java/fr/xebia/confluence2wordpress/core/converter/ConverterTest.java
+++ src/test/java/fr/xebia/confluence2wordpress/core/converter/ConverterTest.java
@@ -1,13 +1,26 @@
+/**
+ * Copyright 2011 Alexandre Dutra
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package fr.xebia.confluence2wordpress.core.converter;
 
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +29,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.atlassian.confluence.core.ContentEntityObject;
+import com.atlassian.confluence.renderer.MacroManager;
 import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.WikiStyleRenderer;
 
@@ -26,6 +40,9 @@ public class ConverterTest {
 	@Mock
 	private WikiStyleRenderer wikiStyleRenderer;
 	
+    @Mock
+    private MacroManager macroManager;
+    
 	@Mock
 	private ContentEntityObject page;
 	
@@ -33,13 +50,12 @@ public class ConverterTest {
 
 	@Before
 	public void createConverter() throws Exception {
-		converter = new Converter(wikiStyleRenderer);
+		converter = new Converter(wikiStyleRenderer, macroManager);
 	}
 
 	@Test
 	public void test_more_macro() throws IOException {
-		String moreMacroHtml = FileUtils.readFileToString(new File("src/main/resources/vm/more.vm"));
-		when(wikiStyleRenderer.convertWikiToXHtml(any(RenderContext.class), any(String.class))).thenReturn(moreMacroHtml);
+		when(wikiStyleRenderer.convertWikiToXHtml(any(RenderContext.class), any(String.class))).thenReturn("<!--more-->");
 		String html = converter.convert(page, new ConverterOptions());
 		assertEquals("<!--more-->", StringUtils.trim(html));
 	}
